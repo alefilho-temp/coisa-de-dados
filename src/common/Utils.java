56891384d3c38076c3e7b2d7006b86a4556d88cb
alefilho-temp <alefilho.temp.mail@gmail.com>
2024-11-24 17:51:14 -0300

    Adição da função de salvar imagem
    Olhar o todo

--- src/common/Utils.java
+++ src/common/Utils.java
@@ -1,7 +1,12 @@
 package common;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.security.SecureRandom;
 
 import javafx.scene.image.Image;
 
@@ -9,6 +14,7 @@ import javafx.scene.image.Image;
  * Classe utilitária que fornece métodos auxiliares para operações comuns.
  */
 public class Utils {
+    private static String imagePrefix = "./";
 
     /**
      * Recupera uma imagem a partir do caminho especificado.
@@ -20,12 +26,56 @@ public class Utils {
      * @return um objeto Image valido
      */
     public static Image getImage(String path) {
-        String prefix = "./";
-
         try {
-            return new Image(new File(prefix + path).toURI().toURL().toString());
+            return new Image(new File(imagePrefix + path).toURI().toURL().toString());
         } catch (MalformedURLException e) {
-            return new Image(prefix + "assets/placeholder.png");
+            return new Image(imagePrefix + "assets/placeholder.png");
         }
+    }
+
+    public static String saveImage(String path) {
+        try {
+            String imageName = generateRandomString(20);
+    
+            Path source = Path.of(path);
+            
+            String fileExtension = "";
+            int lastDotIndex = path.lastIndexOf('.');
+            if (lastDotIndex != -1 && lastDotIndex < path.length() - 1) {
+                fileExtension = path.substring(lastDotIndex + 1);
+            } else {
+                throw new IllegalArgumentException("Invalid file path: no extension found.");
+            }
+    
+            Path destination = Path.of(imagePrefix + "products/" + imageName + "." + fileExtension);
+    
+            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+    
+            return "products/" + imageName + "." + fileExtension;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace(); 
+            return "";
+        }
+    }
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    /**
+     * Gera uma string aleatoria
+     *
+     * @param length Tamanho
+     * @return String
+     */
+    public static String generateRandomString(int length) {
+        StringBuilder stringBuilder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(CHARACTERS.length());
+            stringBuilder.append(CHARACTERS.charAt(index));
+        }
+        return stringBuilder.toString();
     }
 }
